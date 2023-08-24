030f5ffd5384d17005962928a336bbce759c1004
merwinrs <143091868+merwinrs@users.noreply.github.com>
2023-08-24 20:26:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title("My kids are naughty!")
-streamlit.header("Python")
-streamlt.text("Make Changes:")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
